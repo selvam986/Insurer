67e9315c40a8ec27fbcfea8d75db7b2509267b2f
selvam986 <selvamsrinivasan.86@gmail.com>
2019-01-28 15:03:13 -0500

    Added Storyboard & Swift Viewcontroller

---
+++ ios/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Insurer
+//
+//  Created by Selvam on 1/28/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@objc
+class MainViewController: UIViewController {
+  open override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
